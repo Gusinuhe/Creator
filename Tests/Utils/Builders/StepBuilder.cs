3c8a8777a57817e058c938f25768ca43b9932eb7
Gustavo Quiroz <gusinuhe@gmail.com>
2020-08-11 10:31:27 +0200

    feat: package.json was added

--- Tests/Utils/Builders/StepBuilder.cs
+++
@@ -1,9 +0,0 @@
-namespace Innoactive.Creator.Tests.Builder
-{
-    public abstract class StepBuilder<TStep> : BuilderWithResourcePath<TStep>
-    {
-        public StepBuilder(string name) : base(name)
-        {
-        }
-    }
-}
